6fa435c275766555bd1dc4816e3e5f25dead2797
Eric Shi <ericshi98@yahoo.com>
2020-04-07 11:55:46 -0400

    Cleaned up css and div structuring.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
-    // $('#addVideoBtn').click(function() {
-    //     alert('popup modal add video')
-    // })
+    $('#addYtBook').click(function() {
+        alert('popup modal add video')
+    })
+
+    $("#accordion").accordion();
+
 
 })
