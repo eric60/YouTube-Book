510dedd3fc406e20e4d9b7bcb00604843fc0f177
Eric Shi <ericshi98@yahoo.com>
2020-04-14 17:11:56 -0400

    Set up npm package.json in src folder.

---
+++ src/main.ts
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+    let player;
+    let dialogWidth = 800
+    let dialogHeight = 600;
+
+    $('#addYtBook').click(function() {
+       
+    })
+
+    $('.dialog-other').hide();
+    $('#ytUrlInput').bind("paste", function() {
+        handlePaste(insertVideo)
+    });
+
+    function handlePaste(callback) {
+        let url = navigator.clipboard.readText().then(callback);
+    }
+
+    function insertVideo(url) {
+        if(!validateUrl(url)) {
+            alert("Please enter a valid YouTube Video URL");
+        }
+        else {
+            let videoId = parseYoutubeUrl(url)
+            console.log(videoId)
+            onYouTubeIframeAPIReady(player, "player1", videoId);
+            $('.dialog-other').show();
+        }
+    }
+
+    function parseYoutubeUrl(url) {
+        let regEx = /v=([^&]+)/
+        var match = url.match(regEx)
+        console.log(match)
+        let videoId = match[1];
+        return videoId;
+    }
+
+    function validateUrl(url) {
+        // example v=73Fyj6HZ6R0&t=3s
+        // exclude only & character
+        if(!url.match(/v=([^&]+)/)) {
+            return false;
+        }
+        return true;
+    }
+
+    function onYouTubeIframeAPIReady(player, divInsert, videoId) {
+        console.log('trigger youtube player')
+        player = new YT.Player(divInsert, {
+          width: dialogWidth,
+          height: dialogHeight / 2,     
+          videoId: videoId,
+          events: {
+          }
+        });
+      }
+
+    $( "#dialog-add-video" ).dialog({ 
+        autoOpen: false,
+        width: dialogWidth,
+        height:dialogHeight,
+        resizable: false
+    });
+
+    $( "#addVideoBtn" ).click(function() {
+        $( "#dialog-add-video" ).dialog( "open" );
+    });
+
+    $( "#dialog-edit-order" ).dialog({ 
+        autoOpen: false,
+        width: 800,
+        height:600,
+        resizable: false
+    });
+
+    $( "#image-icon" ).click(function() {
+        $( "#dialog-edit-order" ).dialog( "open" );
+    });
+
+    // ================================ Collapsible methods =================================
+    $(".collapsibleBtn catBtn").accordion({
+        collapsible: true
+    });
+
+    
+    $(".collapsibleCategoryBox").accordion({
+        collapsible: true
+    });
+
+    $(".collapsibleBtn labelBtn").accordion({
+        collapsible: true
+    });
+
+    $(".panel-panel-primary-education").accordion({
+        collapsible: true
+    });
+
+    
+
+});
