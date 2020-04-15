1cd05681d38de17afe29c04a9f13df55a1384a6d
Eric Shi <ericshi98@yahoo.com>
2020-04-15 11:09:19 -0400

    Restructured accordions to be sortable. Labels no longer accordions to collapse all videos. Only load 1 category per page.

--- src/main.ts
+++ src/main.ts
@@ -81,12 +81,23 @@ $(document).ready(function() {
     });
 
     // ================================ Collapsible methods =================================
-
-
-    (<any>$(".accordion")).accordion({
+    
+    (<any>$(".Label-Body")).accordion({
+        header: "> div > h3",
         collapsible: true,
         heightStyle: "content"
+    })
+    .sortable({
+        axis: "y",
+        handle: "h3",
+        stop: function( event, ui ) {
+          // IE doesn't register the blur when sorting
+          // so trigger focusout handlers to remove .ui-state-focus
+          ui.item.children( "h3" ).triggerHandler( "focusout" );
+ 
+          // Refresh accordion to handle new order
+          (<any>$( this )).accordion( "refresh" );
+        }
     });
     
-
 });
