2ad7f39631e697656f9fb576426bf504ebd19aad
johnathancampana <johnathancampana@Johnathans-Air>
2020-04-19 19:24:50 -0400

    added some db code from class

--- src/myServer.ts
+++ src/myServer.ts
@@ -1,33 +1,122 @@
-//followed from tutorial here https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb--how-to-get-connected-to-your-database
-
-const {MongoClient} = require('mongodb');
-
-async function main(){
-    
-    // Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect cluster.
-    const uri = "mongodb+srv://<username>:<password>@cs326cluster-0pubh.mongodb.net/test?retryWrites=true&w=majority";
- 
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
+let http = require('http');
+let url = require('url');
+let express = require('express');
+​
+export class MyServer {
+​
+		private theDatabase;
+​
+		// Server stuff: use express instead of http.createServer
+		private server = express();
+		private port = 8080;
+		private router = express.Router();
+​
+		constructor(db) {
+		this.theDatabase = db;
+		// from https://enable-cors.org/server_expressjs.html
+		this.router.use((request, response, next) => {
+			response.header('Content-Type','application/json');
+			response.header('Access-Control-Allow-Origin', '*');
+			response.header('Access-Control-Allow-Headers', '*');
+			next();
+		});
+		// Serve static pages from a particular path.
+		this.server.use('/', express.static('./html'));
+		//// YOUR CODE GOES HERE
+		//// HANDLE CREATE, READ, UPDATE, AND DELETE OPERATIONS
+		//// HANDLE ERRORS WITH A WILDCARD (*)
+		// Start up the counter endpoint at '/counter'.
+		//this.server.use('/counter', this.router);
+​
+		this.router.all('/video/create', this.createHandler.bind(this))
+		this.router.all('/video/*', this.errorHandler.bind(this))
+		this.router.all('/video/read', this.readHandler.bind(this))
+		this.router.all('/video/update', this.updateHandler.bind(this))
+        this.router.all('/video/delete', this.deleteHandler.bind(this))
+        
+        this.router.all('/label/create', this.createHandler.bind(this))
+		this.router.all('/label/*', this.errorHandler.bind(this))
+		this.router.all('/label/read', this.readHandler.bind(this))
+		this.router.all('/label/update', this.updateHandler.bind(this))
+        this.router.all('/label/delete', this.deleteHandler.bind(this))
+        
+        this.router.all('/category/create', this.createHandler.bind(this))
+		this.router.all('/category/*', this.errorHandler.bind(this))
+		this.router.all('/category/read', this.readHandler.bind(this))
+		this.router.all('/category/update', this.updateHandler.bind(this))
+		this.router.all('/category/delete', this.deleteHandler.bind(this))
     }
+​
+    private async errorHandler(request, response, next) : Promise<void> {
+		let name = request.params['username']+"-"+request.query.username
+		console.log("----- in error name: " + name)
+		let value : boolean = await this.theDatabase.isFound(name);
+		console.log('error handler isFound: ' + value)
+		if (!value) {
+			response.write(JSON.stringify({'result' : 'error'}));
+			response.end();
+		} else {
+			next();
+		}
+    }
+    
+    private async createHandler(request, response) : Promise<void> {
+		await this.createCounter(request.params['username']+"-"+ request.query.username, response);
+    }
+​
+    private async readHandler(request, response): Promise<void> {
+		await this.readCounter(request.params['username'] + "-" + request.query.username, response);
+    }
+​
+    private async updateHandler(request, response) : Promise<void> {
+		let value = request.query.value
+		await this.updateCounter(request.params['username'] + "-" + request.query.username, value, response);
+    }
+​
+    private async deleteHandler(request, response) : Promise<void> {
+		await this.deleteCounter(request.params['username'] + "-" + request.query.username, response);
+    }
+​
+    public listen(port) : void  {
+	this.server.listen(port);
+    }
+​/*
+    public async createCounter(name: string, response) : Promise<void> {
+	console.log("creating counter named '" + name + "'");
+	await this.theDatabase.put(name, 0);
+	response.write(JSON.stringify({'result' : 'created',
+				       'name' : name,
+				       'value' : 0 }));
+	response.end();
+    }
+​
+    public async errorCounter(name: string, response) : Promise<void> {
+	response.write(JSON.stringify({'result': 'error'}));
+	response.end();
+    }
+​
+    public async readCounter(name: string, response) : Promise<void> {
+	let value = await this.theDatabase.get(name);
+	response.write(JSON.stringify({'result' : 'read',
+				       'name' : name,
+				       'value' : value }));
+	response.end();
+    }
+​
+    public async updateCounter(name: string, value: number, response) : Promise<void> {
+	await this.theDatabase.put(name, value);
+	response.write(JSON.stringify({'result' : 'updated',
+				       'name' : name,
+				       'value' : value }));
+	response.end();
+    }
+    
+    public async deleteCounter(name : string, response) : Promise<void> {
+	await this.theDatabase.del(name);
+	response.write(JSON.stringify({'result' : 'deleted',
+				       'value'  : name }));
+	response.end();
+    }
+*/
 }
 
-async function listDatabases(client){
-    let databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-main().catch(console.error);
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-// 'use strict';
+'use strict';
 
-// // import { Database } from './mongo-database';
-// import { MyServer } from './myserver';
+import { Database } from './mongo-database';
+import { MyServer } from './myServer';
 
-// const theDatabase = new Database('counter-db-eric60');
-// const theServer = new MyServer(theDatabase);
+const theDatabase = new Database('web-programming-db'); // CHANGE THIS
+const theServer = new MyServer(theDatabase);
 
-// theServer.listen(8080);
+theServer.listen(8080);
